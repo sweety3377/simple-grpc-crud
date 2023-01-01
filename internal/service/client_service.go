fcc7d7f8cf03cba24ba7f3d4db3439ad7b68a813
Alen Babaev <babaev.alen@wb.ru>
2023-01-02 02:31:19 +0300

    Added client service

--- internal/service/client_service.go
+++ internal/service/client_service.go
@@ -1,34 +1,40 @@
 package service
 
-import "net/http"
+import (
+	"context"
+	"simple-grpc-crud/protobuf/pb"
+)
 
-type Repository interface {
-	Create(w http.ResponseWriter, r *http.Request)
-	Read(w http.ResponseWriter, r *http.Request)
-	Update(w http.ResponseWriter, r *http.Request)
-	Delete(w http.ResponseWriter, r *http.Request)
+var _ ClientRepository = (*ClientService)(nil)
+
+type ClientRepository interface {
+	Create(ctx context.Context, in *pb.CreateClientRequest) (*pb.CreateClientResponse, error)
+	Read(ctx context.Context, in *pb.ReadClientRequest) (*pb.ReadClientResponse, error)
+	Update(ctx context.Context, in *pb.UpdateClientRequest) (*pb.UpdateClientResponse, error)
+	Delete(ctx context.Context, in *pb.DeleteClientRequest) (*pb.DeleteClientResponse, error)
 }
 
 type ClientService struct {
-	repository Repository
+	pb.UnimplementedClientGreeterServer
+	repository ClientRepository
 }
 
-func NewClientService(repository Repository) *ClientService {
+func NewClientService(repository ClientRepository) *ClientService {
 	return &ClientService{repository: repository}
 }
 
-func (c *ClientService) Create(w http.ResponseWriter, r *http.Request) {
-	c.repository.Create(w, r)
+func (c *ClientService) Create(ctx context.Context, in *pb.CreateClientRequest) (*pb.CreateClientResponse, error) {
+	return c.repository.Create(ctx, in)
 }
 
-func (c *ClientService) Read(w http.ResponseWriter, r *http.Request) {
-	c.repository.Read(w, r)
+func (c *ClientService) Read(ctx context.Context, in *pb.ReadClientRequest) (*pb.ReadClientResponse, error) {
+	return c.repository.Read(ctx, in)
 }
 
-func (c *ClientService) Update(w http.ResponseWriter, r *http.Request) {
-	c.repository.Update(w, r)
+func (c *ClientService) Update(ctx context.Context, in *pb.UpdateClientRequest) (*pb.UpdateClientResponse, error) {
+	return c.repository.Update(ctx, in)
 }
 
-func (c *ClientService) Delete(w http.ResponseWriter, r *http.Request) {
-	c.repository.Delete(w, r)
+func (c *ClientService) Delete(ctx context.Context, in *pb.DeleteClientRequest) (*pb.DeleteClientResponse, error) {
+	return c.repository.Delete(ctx, in)
 }
